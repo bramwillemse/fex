11fb2e1290ce0c43b7a8c04f8495f786aa2d11ea
Bram Willemse <contact@bramwillemse.nl>
2020-05-18 16:11:02 +0200

    Setup / Fix browsersync reload on change

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,8 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-const replace = require('gulp-replace');
 
 // File paths
 const files = {
@@ -40,18 +38,18 @@ function scriptsTask(){
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask(){
-  watch(files.scripts, series(scriptsTask))
-  watch(files.styles, series(stylesTask))
-  watch(files.html).on('change', reload)
-  watch('./dist/css/*.css').on('change', reload)
-  watch('./dist/js/*.js').on('change', reload)
-
-
   browserSync.init({
     server: {
-      baseDir: "./" // change on your preferred
+      injectChanges: true,
+      baseDir: "./dist" // change on your preferred
     }
   })
+
+  watch(files.scripts, series(scriptsTask))
+  watch(files.styles, series(stylesTask))
+  watch(files.html).on('change', browserSync.reload)
+  watch('./dist/css/*.css').on('change', browserSync.reload)
+  watch('./dist/js/*.js').on('change', browserSync.reload)
 }
 
 exports.default = series(
